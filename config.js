04e8d1f9b316a37a5cf4b86b09edd56679e8e9fe
Nkosi Woseley <nwoseley14@gmail.com>
2016-07-14 21:08:09 -0400

    Added backend functionality

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://user:default@jello.modulusmongo.net:27017/ug7yzEso',
+	// uncomment below and comment above to use localhost instead
+	// 'database': 'mongodb://localhost:27017/node-api',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
